90b21c3212f04b459f5d20e60d79d311a3949aad
Sheila Nicole Cheng <sdcheng@student.apc.edu.ph>
2026-01-16 10:45:43 +0800

    added personal profile

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Fruits</h1>
+  <personal-profile/>
   <food-item/>
   <food-item2/>
 </template>
---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Personal Profile Web Page</h1>
+
+    <h2>About Me</h2>
+<div class="page-wrapper">
+    <div class="profile">
+      <div>Name: Sheila Nicole D. Cheng</div>
+      <div>
+        College undergraduate<br />
+        Course: BS Information Technology<br />
+        IT Experience: Can code with Python, JavaScript, HTML, CSS
+      </div>
+      <div>
+        Hobbies & Interests: Painting, Eating, Walking, Sleeping<br />
+        Goals In Life / Dream: I want to live my life to the fullest
+      </div>
+    </div>
+
+    <h2>Gallery</h2>
+
+    <div class="container">
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTvfwpa0yWEj7tarq1Nfkk0NWRQ-uevnk8LYOd6w62F5JFyJ27xAL9su6bs_qE_4TVens&usqp=CAU"
+          width="200"
+          height="300"
+          alt="Gallery image 1"
+        />
+      </div>
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM4mQ0n9nyUvROIMh7Xa3SdZBAW1JBlWDd0MkaUvk7tiFi7QYDTw4iK8wW5GGLuz2AHXg&usqp=CAU"
+          width="200"
+          height="300"
+          alt="Gallery image 2"
+        />
+      </div>
+      <div>
+        <img
+          src="https://i.pinimg.com/236x/17/d7/ef/17d7efb9372de02f43f14e81b41d7a49.jpg"
+          width="200"
+          height="300"
+          alt="Gallery image 3"
+        />
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style src="./css/PersonalProfile.css"></style>
