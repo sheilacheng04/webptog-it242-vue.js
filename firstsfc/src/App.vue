87b6f35287b5c56a7bdd35d1679fd96d341932b1
Sheila Nicole Cheng <sdcheng@student.apc.edu.ph>
2026-01-16 10:15:43 +0800

    added new food components

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,19 +1,10 @@
 <template>
   <h1>Fruits</h1>
   <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
+  <food-item2/>
 </template>
 
 <script></script>
 
 <style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
 </style>
--- firstsfc/src/components/FoodItem2.vue
+++ firstsfc/src/components/FoodItem2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="oranges">
+  <div class="grapes">
     <h2>{{ name }}</h2>
     <p>{{ message }}</p>
   </div>
@@ -17,11 +17,11 @@ export default {
 </script>
 
 <style>
-  .oranges {
+    .grapes {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: oranges;
+    background-color: purple;
   }
 </style>
